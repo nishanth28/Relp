8e1c873b93df5af7680feb7c0fa549016289229c
nishanth28 <pnishanth28@gmail.com>
2017-02-24 02:49:34 -0500

    GoogleAPI

---
+++ Relp/Restaurant.swift
@@ -0,0 +1,35 @@
+//
+//  Restaurant.swift
+//  Relp
+//
+//  Created by Nishanth P on 2/24/17.
+//  Copyright © 2017 Nishapp. All rights reserved.
+//
+
+import UIKit
+
+class Restaurant: NSObject {
+    
+    var name : String?
+    var lat : Double?
+    var long : Double?
+    var address : String?
+    var open : Bool?
+    var rating : Int?
+    var price : Int?
+    var website : URL?
+    
+    init(name : String, lat : Double, long : Double, address : String, open : Bool, rating : Int, price : Int){
+        
+        self.name = name
+        self.lat = lat
+        self.long = long
+        self.address = address
+        self.open = open
+        self.rating = rating
+        self.price = price
+        self.website = URL(string:"")
+        
+    }
+
+}
--- Relp/RestaurantList.swift
+++ Relp/RestaurantList.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class RestaurantList: UITableViewController {
 
+    let identifer : String = "relpCell"
+    let API : String = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyAZzPfNO8KSatKBRCYaFUjL8WwdcX-ugbk"
+    
+    var restaurants:[Restaurant] = [Restaurant]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        restaurantAPI(API) { (array) in
+            
+            self.restaurants = array
+            self.tableView.reloadData()
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,59 +37,27 @@ class RestaurantList: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return restaurants.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifer , for: indexPath)
+        let restaurant = restaurants[indexPath.row]
+        
+        
+        cell.textLabel?.text = restaurant.name!
 
         return cell
+            
     }
-    */
+    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+    
     /*
     // MARK: - Navigation
 
@@ -92,4 +68,79 @@ class RestaurantList: UITableViewController {
     }
     */
 
+}
+
+
+
+extension RestaurantList{
+    
+    func restaurantAPI(_ urlString:String, completion:@escaping (_ array:[Restaurant]) ->()){
+        
+        
+        var restArray : [Restaurant] = [Restaurant]()
+        var openNow:Bool = false
+        
+        let url = URL(string:urlString)
+        let urlSessionTask = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if error == nil{
+                
+                if let validData = data{
+                    
+                    do {
+                        
+                        let jsonDict = try JSONSerialization.jsonObject(with: validData, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                            let reslist = jsonDict["results"] as! NSArray
+                        for restaurantType in reslist{
+                            
+                            let restaurant = restaurantType as! NSDictionary
+                            
+                            let name = restaurant["name"] as! String
+                            let geometry = restaurant["geometry"] as! NSDictionary
+                            
+                            if let openingHours = restaurant["opening_hours"] as? NSDictionary {
+                                let openNow = openingHours["open_now"] as! Bool
+                            }
+            
+                            let location = geometry["location"] as! NSDictionary
+                            let latitude = location["lat"] as! Double
+                            let longitude = location["lng"] as! Double
+                            
+                            let address = restaurant["vicinity"] as! String
+                            let rating = restaurant["rating"] as? Int ?? 0
+                            let price = restaurant["price_level"] as? Int ?? 0
+                            
+                            let newRestaurant = Restaurant(name:name, lat:latitude, long:longitude, address:address, open:openNow, rating:rating, price:price)
+                            
+                            restArray.append(newRestaurant)
+                            
+                            completion(restArray)
+                        }
+
+                    }
+                    catch {
+                        print(error.localizedDescription)
+                    }//do
+                    
+                }//if
+                
+                
+            }//if
+            
+        }//task
+        
+        urlSessionTask.resume()
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
